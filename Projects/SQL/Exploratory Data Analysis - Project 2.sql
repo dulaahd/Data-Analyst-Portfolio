43caf79c7bf5d16efb920512cd58801d9446c0af
dulaahd <100458030+dulaahd@users.noreply.github.com>
2024-08-03 16:32:50 +0530

    Update Exploratory Data Analysis - Project 2.sql

--- Projects/SQL/Exploratory Data Analysis - Project 2.sql
+++ Projects/SQL/Exploratory Data Analysis - Project 2.sql
@@ -3,55 +3,101 @@
 SELECT *
 FROM layoffs_staging2;
 
+-- to see the maximum total laid off in one day and the percentage of it
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2;
 
+-- To see the Percentage to see how big these layoffs were
+SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off IS NOT NULL;
+
+-- to see which companies laid off all the employees which is the percentage is 1 in Descending order
 SELECT *
 FROM layoffs_staging2
 WHERE percentage_laid_off=1
 ORDER BY total_laid_off DESC;
 
+--to see the size of the companies we order by fund_raised_millions 
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off=1
+ORDER BY funds_raised_millions DESC;
+
+-- to see the top 5 Companies with the biggest Layoff per day
+
+SELECT company, total_laid_off
+FROM layoffs_staging2
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- to see the top 10 companies with most laid of employees
+-- it is grouped by the company and ordered by the 2nd column of the specific table
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
-ORDER BY 2 DESC;
+ORDER BY 2 DESC
+Limit 10;
 
- SELECT MIN(`date`),MAX(`date`)
- FROM layoffs_staging2;
- 
+-- to see the location with most laid of employees
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- what are the 5 countries laid off most of the employees 
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC
+Limit 5;
+
+--what industry laid off most of the employees
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY industry
 ORDER BY 2 DESC;
 
-SELECT country, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY country
-ORDER BY 2 DESC;
 
+
+-- data range of the data that people has laid off
+SELECT MIN(`date`),MAX(`date`)
+FROM layoffs_staging2;
+ 
+
+-- Sum of the laid off employees accordings to years
+-- which is the worst year?
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 2 DESC;
 
-
+-- what are most five stages that laid off?
 SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 2 DESC;
+
+
+
+-- sum is not a good indicator to check percentages since it is not a whole number and it depends on the size of the company
+--therefore Avg would be good to check percentage of laid off employees
 
 SELECT company, AVG(percentage_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- to get the sum of each month in each year
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`,SUM(total_laid_off)
 FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `MONTH`
 ORDER BY 1 ASC;
 
--- ROLLING SUM
+-- To get the ROLLING total of layoffs per month
 
 WITH Rolling_Total AS
 (
@@ -65,22 +111,30 @@ SELECT `MONTH`, total_off,
 SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_Total;
 
+
+
+
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- to see how much laid off per year
 SELECT company, YEAR(`date`),SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company,YEAR(`date`)
 ORDER BY 3 DESC;
 
+-- to rank based on how many companies laid off per year
+-- top 5 companies who laid off people in each year
+-- here there are 2 CTEs used
 WITH Company_Year (company,years,total_laid_off) AS
 (
 SELECT company, YEAR(`date`),SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company,YEAR(`date`)
-), Company_Year_Rank AS
+), 
+Company_Year_Rank AS
 (SELECT *, 
 DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
 FROM Company_Year
@@ -88,7 +142,4 @@ WHERE years IS NOT NULL)
 SELECT *
 FROM Company_Year_Rank
 WHERE Ranking<=5;
-
-
-
 
